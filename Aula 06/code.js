05b0cd6b6fcea742ace416ec6203d518343f31e1
Addams Carvalho <addamscarvalho.sistema@gmail.com>
2025-02-06 16:56:31 -0300

    Add files via upload

---
+++ Aula 06/code.js
@@ -0,0 +1,16 @@
+const marido = 'Addams'; // <--- const 
+// determina uma variável que 
+// nunca terá seu valor alterado.
+// Será constante. Vai dar erro se tentar alterar em outra linha
+
+let esposa ='Izadora'; // let usa-se quando a 
+// variável poderá ser modificada futuramente.
+// Algo como "deixar/livre/etc". Será possível alterar em outras linhas
+var idade; //aqui eu posso definir qual tipo ela será
+var nome ="Luigi";
+
+let mensagem = `Olá, ${esposa}, você é linda.`;
+
+
+console.log(mensagem);
+
